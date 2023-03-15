b1e3256d71a98839d2a5dbc42f9f47a0a20bccc0
X250 <aleksander.synoradzki@gmail.com>
2023-03-15 16:20:29 +0100

    Search menu works, no CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,61 @@
 // importy compomentow do refactoru, niewiem czy destrukturyzacja nie dziala
-import NavBar from './components/navbar/NavBar.js';
-import MovieList from './components/movieList/MovieList.js';
-import SearchMenu from './components/searchmenu/SearchMenu.js';
-import Create from './components/create/Create.js'
-import Edit from './components/edit/Edit.js'
-import './App.css';
-import {useState} from 'react'
+import NavBar from "./components/navbar/NavBar.js";
+import MovieList from "./components/movieList/MovieList.js";
+import SearchMenu from "./components/searchmenu/SearchMenu.js";
+import Create from "./components/create/Create.js";
+import Edit from "./components/edit/Edit.js";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [display, setDisplay] = useState(true)
-  const [create, setCreate] = useState(false)
-  const [edit, setEdit] = useState(false)
-  const [disabledButtons, setDisabledButtons] = useState({
-    display:true,
-    create: false,
-    edit: false
-  })
+    const [display, setDisplay] = useState(true);
+    const [create, setCreate] = useState(false);
+    const [edit, setEdit] = useState(false);
+    const [disabledButtons, setDisabledButtons] = useState({
+        display: true,
+        create: false,
+        edit: false,
+    });
+    const [allFilms, setAllFilms] = useState([]);
+    const [filteredFilms, setFilteredFilms] = useState([]);
 
-  const handleClick = (button) =>  {
-    setDisabledButtons({
-      display: button === 'display',
-      create: button === 'create',
-      edit: button === 'edit'
-    })
-    setDisplay(button === "display")
-    setCreate(button === "create")
-    setEdit(button === "edit")
-  }
+    useEffect(() => {
+        getFilms().catch((err) => alert(err.message));
+    }, []);
 
-  return (
-    <div className="App">
-      <header className="NavBar">
-      <NavBar disabledButtons={disabledButtons} handleClick={handleClick}/>
-      </header>
-        {display && (
-        <div className="Main">
-          <MovieList />
-          <SearchMenu />
-        </div> 
-        )}
-        {create && (
-          <Create />
-        )}
-        {edit && (
-          <Edit />
-        )}
-    </div>
-  );
+    async function getFilms() {
+        const data = await fetch('http://127.0.0.1:3001/api/movies');
+        const films = await data.json();
+        setAllFilms(films);
+        // setFilteredFilms(films);
+    }
+
+    const handleClick = (button) => {
+        setDisabledButtons({
+            display: button === "display",
+            create: button === "create",
+            edit: button === "edit",
+        });
+        setDisplay(button === "display");
+        setCreate(button === "create");
+        setEdit(button === "edit");
+    };
+
+    return (
+        <div className="App">
+            <header className="NavBar">
+                <NavBar disabledButtons={disabledButtons} handleClick={handleClick} />
+            </header>
+            {display && (
+                <div className="Main">
+                    <MovieList />
+                    <SearchMenu allFilms={allFilms} filteredFilms={filteredFilms} setFilteredFilms={setFilteredFilms} />
+                </div>
+            )}
+            {create && <Create />}
+            {edit && <Edit />}
+        </div>
+    );
 }
 
 export default App;
--- client/src/components/searchmenu/SearchMenu.js
+++ client/src/components/searchmenu/SearchMenu.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import "./SearchMenu.css";
+import React from "react";
+import { useState, useEffect } from "react";
 
-const SearchMenu = () => {
-  return (
-    <h1>SearchMenu Placeholder</h1>
-  )
-}
+const SearchMenu = ({ allFilms, filteredFilms, setFilteredFilms }) => {
+    const [localFilteredFilms, setLocalFilteredFilms] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+    let allGenres = getFilmGenres(allFilms);
+
+    useEffect(() => {
+        if (allFilms) {
+            setLocalFilteredFilms(allFilms);
+            setFilteredFilms(allFilms);
+        }
+    }, [allFilms]);
+
+    function getFilmGenres(films) {
+        const allGenres = new Set();
+        films.forEach((film) => {
+            film.genres.forEach((genre) => {
+                allGenres.add(genre.toLowerCase());
+            });
+        });
+        return [...allGenres];
+    }
+
+    function handleOnchange(e) {
+        // console.log(filteredFilms);
+        setInputValue(e.target.value);
+        const filtered = localFilteredFilms.filter((film) => {
+            const filmTitleLowerCase = film.title.toLowerCase();
+            const searchedStringLowerCase = e.target.value.toLowerCase();
+            return filmTitleLowerCase.includes(searchedStringLowerCase);
+        });
+        // console.log(filtered);
+        setFilteredFilms(filtered);
+    }
+
+    function handleAllClick() {
+        setLocalFilteredFilms(allFilms);
+        setFilteredFilms(allFilms);
+        setInputValue("")
+    }
+
+    function handleGenreBtnClick(genre) {
+        const filtered = allFilms.filter((film) => {
+            const lowerCaseGenres = film.genres.map((genre) => genre.toLowerCase());
+            return lowerCaseGenres.includes(genre);
+        });
+        setLocalFilteredFilms(filtered);
+        setFilteredFilms(filtered);
+        setInputValue("");
+    }
+
+    return (
+        <div className="SearchMenu">
+            <form>
+                <input className="input" name="title" type="text" placeholder="title" value={inputValue} onChange={handleOnchange} />
+            </form>
+            <div className="FilterButtons">
+                <button onClick={handleAllClick}>all</button>
+                {allGenres.map((genre, index) => (
+                    <button key={index} onClick={()=>handleGenreBtnClick(genre)}>{genre}</button>
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default SearchMenu;
